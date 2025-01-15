4273e51d02d795c87c497cd281bf5759f2faf2ae
toek0604 <adamaziz0629@gmail.com>
2025-01-15 10:54:05 +0700

    nambah tugas

---
+++ latihan_2/soal1.py
@@ -0,0 +1,6 @@
+#program menentukan nilai siswa
+nilai= int(input("masukan niai siswa :"))
+if nilai >=75 :
+    print("siswab lulus")
+else:
+    print("siswa tidak lulus")
---
+++ latihan_2/soal2.py
@@ -0,0 +1,10 @@
+#program menentukan tinggi badan untuk menaiki wahana
+print("wahana dunia fantasi")
+tinggi=int(input("masukan tinggi siswa :"))
+
+#mengecek
+if tinggi > 160:
+    print("boleh menaiki wahana")
+else:
+    print("tidak boleh menaiki wahana")
+    
